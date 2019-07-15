22d3b34f9cb231717af9bf3b9977a4c1b19692f0
mrpantoni <46929644+mrpantoni@users.noreply.github.com>
2019-07-15 11:27:06 +0200

    Add files via upload

---
+++ Factorial/FactorialExercise/FactorialCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialExercise.Exercises
+{
+    //Készítsünk rekurzív faktoriális és hatványt számító függvényeket!
+    public static class FactorialCalculator
+    {
+        public static int FactorialRecursion(int factNum)
+        {
+            if (factNum < 0)
+            {
+                throw new ArgumentException("Only Positive number accepted!");
+            }
+            else if (factNum == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return factNum * FactorialRecursion(factNum - 1);
+            }
+        }
+
+        public static double ExponentiationRecursion(double baseNum, int exponentNum)
+        {
+            if (baseNum < 0 || exponentNum < 0)
+            {
+                throw new ArgumentException("Only Positive number accepted!");
+            }
+            else if (exponentNum == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseNum * ExponentiationRecursion(baseNum, exponentNum - 1);
+            }
+        }
+    }
+}
---
+++ Factorial/FactorialExerciseTests/FactorialCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using FactorialExercise.Exercises;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RecursionExercise.Tests
+{
+    [TestClass]
+    public class FactorialTest
+    {
+        [TestMethod]
+        public void TestFactorialCalculation()
+        {
+            Assert.AreEqual(1, FactorialCalculator.FactorialRecursion(1));
+            Assert.AreEqual(6, FactorialCalculator.FactorialRecursion(3));
+            Assert.AreEqual(120, FactorialCalculator.FactorialRecursion(5));
+        }
+
+        [TestMethod]
+        public void TestHatvanyCalculator()
+        {
+            Assert.AreEqual(1, FactorialCalculator.ExponentiationRecursion(2, 0));
+            Assert.AreEqual(256, FactorialCalculator.ExponentiationRecursion(2, 8));
+            Assert.AreEqual(19683, FactorialCalculator.ExponentiationRecursion(3, 9));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestHatvanyCalculato()
+        {
+            FactorialCalculator.FactorialRecursion(-3);
+            FactorialCalculator.ExponentiationRecursion(-2, 8);
+        }
+    }
+}
